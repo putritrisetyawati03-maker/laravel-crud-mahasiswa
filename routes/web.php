2c4f384320b064f26173f0385d800f23b8686659
putritrisetyawati03-maker <putritrisetyawati03@gmail.com>
2026-02-07 17:28:03 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
+
+// Route untuk halaman welcome (default Laravel)
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/cv', 'cv');
+Route::resource('mahasiswa', MahasiswaController::class);
+
